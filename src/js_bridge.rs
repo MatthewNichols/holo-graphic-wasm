efd6d55ee39b420bbd61322a58ea38c55291a2c8
Matthew Nichols <matthew@matthew-nichols.com>
2020-03-26 20:24:52 +0100

    Initial Commit. WASM writes to Canvas.

---
+++ src/js_bridge.rs
@@ -0,0 +1,14 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen(module = "/js/canvas-writer.js")]
+extern {
+    pub fn sayHello(name: &str);
+    pub fn init(canvasId: &str, height: i32, width: i32);
+    pub fn clear(colorCode: &str);
+    pub fn drawCircle(centerX: i32, centerY: i32, radius: i32, color: &str);
+}
---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use wasm_bindgen::prelude::*;
+use web_sys::console;
+
+mod js_bridge;
+
+// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
+// allocator.
+//
+// If you don't want to use `wee_alloc`, you can safely delete this.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+
+// This is like the `main` function, except for JavaScript.
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(debug_assertions)]
+    console_error_panic_hook::set_once();
+
+
+    // Your code goes here!
+    console::log_1(&JsValue::from_str("Hello world!"));
+    
+    js_bridge::init("canvas1", 1000, 1000);
+    js_bridge::clear("#000");
+    js_bridge::drawCircle(200, 200, 100, "#ff0000");
+
+
+    Ok(())
+}
